d457d52f290be382d28d4800b29929f451d5a577
siavash sattari <siavash.sattari.dev@gmail.com>
2023-02-22 01:49:08 -0800

    feat - pages and routes

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <>
+      <h1 className='text-6xl mb-4'>Github Finder</h1>
+      <p className='mb-4 text-2xl font-light'>A React app to search GitHub profiles and see profile details.</p>
+    </>
+  );
+};
+
+export default About;
